e86eb7dc9b9d426ae62cf2b4e3b488cec033d3fe
corbamico <corbamico@163.com>
2013-11-28 17:37:50 +0800

    update:save gps to sqlite

--- perl/gt02a_tcp_server.pl
+++ perl/gt02a_tcp_server.pl
@@ -23,31 +23,76 @@
 
 use strict;
 use IO::Socket;
+use DBI;
 
 
 my $socket_port = 7789;
+my $dbfile = "gpstrack.db3";
 
 sub parsePacket($);
 sub HEART(){return pack("C*",0x54,0x68,0x1A,0x0D,0x0A);}
 
+
 my $client; #socket client
 my $data;   #data from client one read
-my $server = 
-   IO::Socket::INET->new(LocalPort => $socket_port,
-                         Type      => SOCK_STREAM,
-                         Reuse     =>1,
-                         Listen    =>3)
-    or die "Counld creae tcp server ";            
-        
-while ($client = $server->accept()){
-    while($data = <$client>){
-        #we got data from here.
-        parsePacket($data);
-    }
-    $client->close();
+my $server; #socket server
+my $dbh;    #DB hancle
+my $sth;    
+#my $imei,$date,$lng,$lat,$speed,$direction,$resevered,$status;
+
+
+
+#  
+#  name: convertGPSData
+#  convert string to GPS Data
+#  @param  string format($imei,$date,$lng,$lat,$speed,$direction,$status)
+#  @return db     format($imei,$date,$lng,$lat,$speed,$direction,$status)
+#  
+sub convertGPSData(@)
+{
 }
 
-$server->close();
+#($imei,$date,$lng,$lat,$speed,$direction,$status)
+sub saveGPSData(@)
+{
+	$sth->execute(@_);
+	
+    #$sth->bind_param(1, $date, { TYPE => SQL_DATETIME });
+	#$sth->bind_param(2, $imei, { TYPE => SQL_INTEGER });
+	#$sth->bind_param(3, $lng, { TYPE => SQL_FLOAT });
+	#$sth->bind_param(4, $lat, { TYPE => SQL_FLOAT });
+	#$sth->bind_param(5, $speed, { TYPE => SQL_INTEGER });
+	#$sth->bind_param(6, $direction, { TYPE => SQL_INTEGER });
+	#$sth->bind_param(7, $status, { TYPE => SQL_INTEGER });
+}
+
+sub init
+{
+	$server= 
+	   IO::Socket::INET->new(LocalPort => $socket_port,
+							 Type      => SOCK_STREAM,
+							 Reuse     =>1,
+							 Listen    =>3)
+		or die "Counld not create tcp server ";  
+	
+	$dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","") 
+		or die "Counld not connect to sqlite3 DB"; 
+	$sth = $dbh->prepare(qq{insert into gpstrack values(?,?,?,?,?,?,?)});
+	
+}
+sub run
+{
+         
+	while ($client = $server->accept()){
+		while($data = <$client>){
+			#we got data from here.
+			parsePacket($data);
+		}
+		$client->close();
+	}
+
+	$server->close();
+}
 
 sub parsePacket($){
     my ($data) = unpack "H*", $_[0] ;  #converto '546681a0d0a' etc
@@ -60,25 +105,33 @@ sub parsePacket($){
     #we got gps data from client
     elsif ($data =~ /^6868250000.{10}10.*0d0a$/ )
     {
-          my ($imei,$date,$lng,$lat,$speed,$direction,$resevered,$status) 
-        = ($data =~ /^6868250000(.{8})..10(.{6})(.{4})(.{4})(.{1})(.{2})(.{3})(.{4})0d0a$/)
+        my ($imei,$date,$lng,$lat,$speed,$direction,$resevered,$status) 
+          = ($data =~ /^6868250000(.{8})..10(.{6})(.{4})(.{4})(.{1})(.{2})(.{3})(.{4})0d0a$/);
+        my @param  = ($imei,$date,$lng,$lat,$speed,$direction,$status);
+        my @result = convertGPS(@param);
+        saveGPSData(@result);
     }
     
     #we don't known other data format, if we get here.
 }
 
-=begin
-==head 1.Packet
+
+init();
+run();
+
+
+__END__
+==head1 1.Packet
 Data From Client: 68 68 25  00 00 ID-8byte SEQ-2byte 10 ....... 0D 0A
 Hear From Client: 68 68 len xx xx ID-8byte SEQ-2byte 1A ....... 0D 0A
 
 ID-8byte(IMEI): 01 23 45 67 89 12 34 50
 
-==head 2.Heart beat
+==head1 2.Heart beat
 heart_from_server = (0x54,0x68,0x1A,0x0D,0x0A);
 heart_from_client = (0x68,0x68,..., 0x1A,...,0x0D,0x0A);
 
-==head 3.Data 
+==head1 3.Data 
 Data from Client (24byte):
 Date Lng Lat Speed Direction resevered Status
  6    4   4     1     2          3        4
@@ -95,4 +148,16 @@ Status:
   BIT 3: 0/1 Power Charge(No/Yes)
   BIT 4: 0/1 Normal/SoS
   BIT 5: 0/1 Normal/Force Poweroff alert
-=end
+
+==head1 4.SQLite Table gpstrack
+create table gpstrack 
+(
+  date datetime not null,
+  imei char(16) not null,
+  lng  float default 0.0,
+  lat  float default 0.0,
+  speed integer default 0,
+  direction integer default 0,
+  status integer default 0,
+  primary key (date,imei)
+);
